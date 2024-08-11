1e0b0549f057a516b4faa4be867a02037f7b6ca6
Oskar Sałata <126605059+iXpleew@users.noreply.github.com>
2024-08-11 20:43:37 +0200

    Update IncomeSceneBuild.java
    connecting income integer to the database

--- bugdetprojecy/IncomeSceneBuild.java
+++ bugdetprojecy/IncomeSceneBuild.java
@@ -9,6 +9,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class IncomeSceneBuild {
     private Scene mainScene;
     private Stage primaryStage;
@@ -17,13 +19,11 @@ public class IncomeSceneBuild {
         this.primaryStage = primaryStage;
         this.mainScene = mainScene;
     }
-    public Scene buildIncomeScene(){
+    public Scene buildIncomeScene() throws IOException {
         AnchorPane incomePane = new AnchorPane();
         Scene incomeScene = new Scene(incomePane, 800, 400);
 
-        //dlaczego nie moge dac static tutaj? a bardzo potrzebuje aby sie zapisalo bo jak znowu wchodze na ta scene t osie nie zapisuje
-        //static int income = 0;
-
+        final int[] income = {Integer.parseInt(FileManagment.FileChecker(1))};
         Button backToMenu = new Button("Back");{
             backToMenu.setLayoutX(50);
             backToMenu.setLayoutY(30);
@@ -44,22 +44,25 @@ public class IncomeSceneBuild {
             enterIncomeTField.setLayoutX(20);
             enterIncomeTField.setLayoutY(110);
         }
-        Text yourIncomeText = new Text("Your actual net income: 0");{
+        Text yourIncomeText = new Text("Your actual net income: " + income[0]);{
             yourIncomeText.setLayoutX(20);
-            yourIncomeText.setLayoutY(130);
+            yourIncomeText.setLayoutY(150);
         }
         Button applyIncome = new Button("Apply");{
             applyIncome.setLayoutX(180);
             applyIncome.setLayoutY(110);
 
+
             applyIncome.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
-
+                    // o tutaj
+                    income [0] = Integer.parseInt(enterIncomeTField.getText());
+                    FileManagment.FileChanger(1, "Net income: " + income[0]);
+                    yourIncomeText.setText("Your actual net income: " + income[0]);
                 }
             });
         }
-
         incomePane.getChildren().addAll(backToMenu, enterIncomeText, enterIncomeTField, applyIncome, yourIncomeText);
 
         return incomeScene;
