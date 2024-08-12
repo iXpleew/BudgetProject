52b553eede902b6017becae5adabcb89ef2e03b0
Oskar Sałata <126605059+iXpleew@users.noreply.github.com>
2024-08-12 19:25:20 +0200

    Update Expense.java
    adding variable with type LocalDate to the class

--- bugdetprojecy/Expense.java
+++ bugdetprojecy/Expense.java
@@ -1,12 +1,16 @@
 package com.example.bugdetprojecy;
 
+import java.time.LocalDate;
+
 public class Expense {
     private final String nameOfExpense;
     private final int amountOfExpense;
+    private final LocalDate dateOfExpense;
 
-    public Expense(String nameOfExpense, int amountOfExpense) {
+    public Expense(String nameOfExpense, int amountOfExpense, LocalDate dateOfExpense) {
         this.nameOfExpense = nameOfExpense;
         this.amountOfExpense = amountOfExpense;
+        this.dateOfExpense = dateOfExpense;
     }
     public String getNameOfExpense() {
         return nameOfExpense;
@@ -14,4 +18,5 @@ public class Expense {
     public int getAmountOfExpense() {
         return amountOfExpense;
     }
+    public LocalDate getDateOfExpense(){return dateOfExpense;}
 }
